5948224f08f2ae73ecb5a9a729d961cdd44a18da
onur <onrkrdmn@gmail.com>
2018-03-21 23:55:52 +0100

    Adding prod properties and ssl configuration

--- kassandra-frontend/src/app/core/providers/news/news-provider.service.ts
+++ kassandra-frontend/src/app/core/providers/news/news-provider.service.ts
@@ -3,7 +3,7 @@ import {Http, ResponseContentType, Response} from "@angular/http";
 import {Type, GET, Query, BaseUrl, AbstractApiClient} from "angular2-rest-client";
 import {Observable} from "rxjs";
 
-@BaseUrl('http://45.62.231.206:8080/api/v1.0.0')
+@BaseUrl('https://45.62.231.206:8080/api/v1.0.0')
 
 @Injectable()
 export class NewsProviderService extends AbstractApiClient {
